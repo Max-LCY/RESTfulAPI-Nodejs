14db8d01c6d2e2ebedd6e330aab6ce2520dd97db
Ivan <maxlcy-work@outlook.com>
2021-07-17 12:40:19 +0000

    initial setting

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import config from './config/config'
+import app from './config/express'
+
+if (module.children) {
+  // listen on port config.port
+  app.listen(config.port, () => {
+    console.log(
+      `server started on  port http://127.0.0.1:${config.port} (${config.env})`
+    )
+  })
+}
+
+export default app
